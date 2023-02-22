126edb65d0e9e86c8c89550a8c2125ee597e6c4d
JosephBmbs <youssefbmbs@gmail.com>
2023-02-22 06:51:39 -0800

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+* main - print the string in the put function
+*
+* Description: using the printf function
+* this program prints " with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
 int main()
 {
    // printf() displays the string inside quotation
