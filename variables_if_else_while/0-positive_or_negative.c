9fbeff19b97513c6a54c9ebd20383a14fb5e2387
JosephBmbs <youssefbmbs@gmail.com>
2023-02-24 10:13:39 -0800

    0-positive_or_negative.c fix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main – This program will assign a random number to the variable n
- *
- * Return: Always 0 (Success)
- */
+* main – This program will assign a random number to the variable n
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand() – RAND_MAX / 2;
 	if (n == 0
 		printf("%i is zero\n", n);
 	else if (n < 0)
