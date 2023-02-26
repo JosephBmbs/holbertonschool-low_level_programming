04ad3c177e61c7cab2f9246f7fcf066af8e55f68
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-02-26 15:49:18 +0100

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase, followed by a new line
+*
+*
+* Return: 0
+**/
+
+int main(void)
+{
+    char NUM;
+
+    for (NUM = 'a'; NUM <= 'z'; NUM++)
+            putchar(NUM);
+    putchar('\n');
+
+    return (0);        
+    
+}
