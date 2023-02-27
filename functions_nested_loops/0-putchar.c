8de122d2ebd5f0b7ab3743de79dafcb8b91589d9
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-02-27 11:37:49 +0100

    0-putchar.c on dev

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main – a program that prints _putchar, followed by a new line.
+* 
+* Return: always 0
+**/
+
+int main (void)
+{
+putchar('\_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('\n');
+return 0;            
+}
