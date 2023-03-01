814eabdc4576f0286d6a87916b7efe93546292fd
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-03-01 10:52:42 +0100

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /** 
-* main - prints the alphabet, in lowercase, followed by a new line
+* print_alphabet - prints the alphabet, in lowercase, followed by a new line
 *
 * Return: Always 0.
 */
+
 void print_alphabet(void)
 {
-    int i;
-    int u;
-    i = 1;
-    u = 97;
-    while (i < 27)
-    {
-        _putchar(u);
-        i++;
-        u++;
-    }
-        _putchar('\n'); 
+int i;
+int u;
+i = 1;
+u = 97;
+while (i < 27)
+{
+_putchar(u);
+i++;
+u++;
+}
+_putchar('\n'); 
 }
