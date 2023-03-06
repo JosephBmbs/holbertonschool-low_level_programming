7b484ab09a9da8e4f537bc5e4362a4ca197cc61f
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-03-06 15:21:23 +0100

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,23 +15,22 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-		    	printf("%d", i);
-			    if (i != 98)
-		    	{
-			    	printf(", ");
-			    }
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
-	}
-    else
+	} else
 	{
 		for (i = n; i <= 98; i++)
 		{
-			    printf("%d", i);
-			    if (i != 98)
-			    {
-				    printf(", ");
-		    	}
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
-	        printf("\n");
+	printf("\n");
 }
