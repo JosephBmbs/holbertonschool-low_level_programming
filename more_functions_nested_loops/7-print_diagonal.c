f2ee4b49c39075bb3ce920866aa059e86fa9f735
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-04-05 03:33:56 +0100

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal
+ *
+ * @n: integer to set diagonal
+ */
+void print_diagonal(int n)
+{
+int i;
+int x;
+if (n <= 0)
+{
+_putchar('\n');
+} else
+{
+for (i = 0; i < n; i++)
+{
+for (x = 0; x < i; x++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
