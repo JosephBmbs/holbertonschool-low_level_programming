9d0054da2dc246b6b2a6a92632a8535cceb2135e
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-04-05 03:39:06 +0100

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square, followed by a new line
+ * @size: integer to set square size
+ */
+void print_square(int size)
+{
+int i;
+int x;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (x = 0; x < size; x++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
