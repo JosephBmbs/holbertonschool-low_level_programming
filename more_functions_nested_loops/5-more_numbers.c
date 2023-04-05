bdb51f5d5740b2b56a14230eb70f0e2c92d08980
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-04-05 02:45:52 +0100

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* more_numbers - unction that prints 10 times the numbers, from 0 to 14
+*
+* Return: Always (0)
+*/ 
+
+void more_numbers(void)
+{
+int r;
+int num;
+for (r = 0; r <= 9; r++)
+{
+	for (num = 0; num <= 14; num++)
+	{
+			if (num > 9)
+				{
+					_putchar(num / 10 + '0');
+				}
+		
+		_putchar ((num % 10) + '0');
+		
+	}
+	
+	_putchar (r);
+	_putchar('\n');
+	
+}
+}
