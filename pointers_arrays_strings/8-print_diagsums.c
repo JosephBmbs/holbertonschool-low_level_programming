108dd3e6b35c403d0445b167eea489b6f7df0940
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-04-18 05:21:12 +0100

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,14 @@
 
 void print_diagsums(int *a, int size)
 {
-		int i;
-		int diagO = 0;
-		int diagT = 0;
+	int i;
+	int diagO = 0;
+	int diagT = 0;
 
 	for (i = 0; i < size; i++)
 	{
-				diagO += a[(i * size) + i];
-				diagT += a[(size - 1) + ((size - 1) * i)];
+	diagO += a[(i * size) + i];
+	diagT += a[(size - 1) + ((size - 1) * i)];
 	}
-		printf("%d, %d\n", diagO, diagT);
+	printf("%d, %d\n", diagO, diagT);
 }
