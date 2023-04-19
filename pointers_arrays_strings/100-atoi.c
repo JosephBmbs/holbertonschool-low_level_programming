830095c676f9682afa389a6a4fa27ab56f007ac5
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-04-19 06:21:34 +0100

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,41 +9,20 @@
 
 int _atoi(char *s)
 {
-int i = 0, j, din;
-int sign = 1, bdet = 0, wfet = 0, bdina = 0, out = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
-	{
-		if (bdina != 0)
-		{
-			if ((s[i] - '0' < 0) || (s[i] - '0' > 9))
-			{
-				wfet = i - 1;
-				break;
-			}
-		}
-		else
-		{
-			if (s[i] == '-')
-				sign *= -1;
-			if ((s[i] - '0' >= 0) && (s[i] - '0' <= 9))
-			{
-				bdina = 1;
-				bdet = i;
-			}
-		}
-		i++;
-	}
-	wfet = (wfet == 0) ? i - 1 : wfet;
-	if (bdina)
-	{
-		for (i = bdet; i <= wfet; i++)
-		{
-			din = 1;
-			for (j = 0; j < (wfet - i); j++)
-				din *= 10;
-			out += din * (s[i] - '0');
-		}
-	}
-	return (out *= sign);
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
