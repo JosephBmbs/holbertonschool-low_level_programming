982af8fcc84fb6e8b8f29e2f28f0c7c1de5c4465
JosephBmbs <abdel@Bmbs-MacBook-Pro.local>
2023-04-22 07:04:31 +0100

    the last solution

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MY_H
+#define MY_H
 
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int _isupper(int c);
-int _isdigit(int c);
+int _isupper(char c);
+int _isdigit(char c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif /* MY_H */
