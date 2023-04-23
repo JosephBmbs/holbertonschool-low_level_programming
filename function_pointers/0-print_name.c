512647f92f0ab6227b4267f45e00e505fb4d0a2a
Youssef_Saad <6270@holbertonstudents.com>
2023-04-23 07:18:26 +0100

    fixing betty stile

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,13 +5,10 @@
  * print_name - a function that prints a name
  * @name: name to print
  * @f: fanction to use
- *
  */
 
 void print_name(char *name, void (*f)(char *))
-
 {
-	if (name && f != NULL)
-
-	f(name);
+if (name && f != NULL)
+f(name);
 }
