7d9a6a638cdc09a91a8819ec7df6b44dfa4d530f
Youssef_Saad <6270@holbertonstudents.com>
2023-04-26 09:04:46 +0100

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t count;
+	size_t count;
 
-    for (count = 0; h != NULL; count++)
-    {
-        printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
-        h = h->next;
-    }
+	for (count = 0; h != NULL; count++)
+	{
+		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
+		h = h->next;
+	}
 
-    return count;
+	return count;
 }
