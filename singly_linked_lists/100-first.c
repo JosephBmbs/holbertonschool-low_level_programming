cf9961d9f22fa19b65ed4d12d65299ee1f9c55ab
Youssef_Saad <6270@holbertonstudents.com>
2023-05-01 06:13:34 +0100

    100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,12 +3,13 @@
 void __attribute__((constructor)) before_main();
 /**
  * _constructor - excutes befor main
- *
+ * -befor_main - print out a specific message before
+ * the main function is executed
  * Return: void
  */
 
 void __attribute__((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\n"
+	printf("You're beat! and yet, you must allow,\n"
 	"I bore my house upon my back!\n");
 }
