3258d427c12423813a037414a90965cf3247293d
Youssef_Saad <6270@holbertonstudents.com>
2023-05-01 06:18:06 +0100

    100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,13 +3,15 @@
 void __attribute__((constructor)) before_main();
 /**
  * _constructor - excutes befor main
- * befor_main - print out a specific message before
+ * hare - Prints a string before the
  * the main function is executed
  * Return: void
  */
 
-void __attribute__((constructor)) before_main()
+void __attribute__((constructor)) hare(void);
+
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-	"I bore my house upon my back!\n");
+	       "I bore my house upon my back!\n");
 }
