47a2088ff5e02ef46804b9a2663d2911fdfac5c8
Youssef_Saad <6270@holbertonstudents.com>
2023-05-01 06:08:21 +0100

    100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
+#include "lists.h"
 
+void __attribute__((constructor)) before_main();
 /**
  * _constructor - excutes befor main
  * 
  * Return: void
  */
 
-void __attribute__((constructor)) before_main()
+void _constructor(void)
 {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-int main(void)
-{
-printf("This is the main function.\n");
-return 0;
 }
