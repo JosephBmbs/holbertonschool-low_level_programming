55fd50d6f23b607972a63ff6a39ebe94465f404b
Youssef_Saad <6270@holbertonstudents.com>
2023-05-01 05:46:16 +0100

    1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -12,7 +12,8 @@ size_t dlistint_len(const dlistint_t *h)
 {
 size_t count = 0;
 const dlistint_t *current = h;
-while (current != NULL) {
+while (current != NULL)
+{
 count++;
 current = current->next;
 }
