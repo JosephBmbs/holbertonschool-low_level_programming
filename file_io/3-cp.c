1ec98ea8ef0433d7eec067568a34377227aee65b
Youssef_Saad <6270@holbertonstudents.com>
2023-05-02 16:29:35 +0100

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,7 +30,8 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		 argv[1]), exit(98);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
@@ -39,7 +40,8 @@ int main(int argc, char *argv[])
 	do {
 		r = read(fd_from, buffer, BUFFER_SIZE);
 		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			 argv[1]), exit(98);
 		w = write(fd_to, buffer, r);
 		if (w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
