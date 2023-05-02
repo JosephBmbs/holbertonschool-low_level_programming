379445d119f9ac95427196285abe7fff2659cb5b
Youssef_Saad <6270@holbertonstudents.com>
2023-05-02 14:52:54 +0100

    3-set_bit.c

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * set_bit -change the value of a given bit index to 1
+ * @n:pointer to long int
+ * @index: index change position
+ * Return: 1if it worked or -1 if it failed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	return (-1);
+	*n = (1 << index) | *n;
+	return (1);
+}
