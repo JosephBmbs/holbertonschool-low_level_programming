fd4a718fd900422a5a6b26c7130d1af3a1d9430a
Youssef_Saad <6270@holbertonstudents.com>
2023-05-03 13:01:14 +0100

    4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -19,16 +19,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
 	node = ht->array[index];
-
-	while (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-
+	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
-	}
 
-	return (NULL);
+	return ((node == NULL) ? NULL : node->value);
 }
-
