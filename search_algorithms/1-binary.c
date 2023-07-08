1e6f5ae2933e1f48284d8fcba51e60b3450cdcdd
Youssef_Saad <6270@holbertonstudents.com>
2023-07-08 19:59:00 +0100

    1-binary.c fixing the kernel style code

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -3,13 +3,15 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array of integers using binary search.
+ * binary_search - Searches for a value in a sorted
+ * array of integers using binary search.
  *
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: The value to search for.
  *
- * Return: The index where the value is located, or -1 if not found or array is NULL.
+ * Return: The index where the value is located, or -1 if
+ * not found or array is NULL.
  *
  * Description: Prints the array being searched every time it changes.
  */
